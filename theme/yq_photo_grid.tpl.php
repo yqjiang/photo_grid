510cb02c15049b1f3501574578993026a89a2215
yqjiang <you@example.com>
2013-10-21 09:15:02 -0300

    update main part. tpl and js need to be update.

---
+++ theme/yq_photo_grid.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * photowall.tpl.php
+ *
+ * Theme implementation to display photowall.
+ */
+dsm($variables,'templete');
+?>
+
+<div id="yq_photo_grid">
+  <div class="body">
+  </div>
+</div>
